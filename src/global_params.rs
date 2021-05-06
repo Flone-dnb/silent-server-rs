ec28117354a81340e6e1d9acd4d853266b61e286
Flone-dnb <flonednb@gmail.com>
2021-05-06 20:45:43 +0500

    Change param.

--- src/global_params.rs
+++ src/global_params.rs
@@ -3,4 +3,4 @@ pub const LOG_FILE_NAME: &str = "silent_server.log";
 pub const SERVER_DEFAULT_PORT: u16 = 51337;
 pub const SUPPORTED_CLIENT_VERSION: &str = "0.1.0";
 pub const MAX_VERSION_STRING_LENGTH: u32 = 30;
-pub const INTERVAL_TCP_MESSAGE_MS: u64 = 200;
+pub const INTERVAL_TCP_MESSAGE_MS: u64 = 250;
